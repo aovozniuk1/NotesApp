6ef611c87d380b0fcdd16d120a41b159c6e30230
Andrii <a.o.vozniuk@gmail.com>
2021-08-17 16:02:27 +0300

    update

--- app/src/main/java/learn/android/notesapp/MainActivity.java
+++ app/src/main/java/learn/android/notesapp/MainActivity.java
@@ -1,6 +1,7 @@
 package learn.android.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -16,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
                     .beginTransaction()
                     .replace(R.id.notes_list_container,NotesFragment.newInstance())
                     .commit();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ищем фрагмент, который сидит в контейнере R.id.cities_container
+        Fragment backStackFragment = (Fragment)getSupportFragmentManager()
+                .findFragmentById(R.id.notes_list_container);
+        // если такой есть, и он является CoatOfArmsFragment
+        if(backStackFragment!=null&&backStackFragment instanceof NoteDescriptionFragment){
+            //то сэмулируем нажатие кнопки Назад
+            onBackPressed();
         }
     }
 }
